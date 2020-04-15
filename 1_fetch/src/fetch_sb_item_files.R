c46ce285b4c0748d98aedd6bfed4568de6f9b954
Platt <lplatt@usgs.gov>
2020-04-15 08:44:26 -0500

    update fetch to get clarity + hyspo + lake-group xwalk & improve sb fetching fxns

--- 1_fetch/src/fetch_sb_item_files.R
+++ 1_fetch/src/fetch_sb_item_files.R
@@ -1,19 +1,43 @@
 list_sb_files <- function(target_name, sb_id, keyword) {
   
+  sb_check_login()
+  
   sb_files <- item_list_files(sb_id)[["fname"]]
   keyword_sb_files <- sb_files[grepl(keyword, sb_files)]
 
   saveRDS(object = keyword_sb_files, file = target_name)
 }
 
-download_sb_files <- function(target_name, sb_id, sb_filenames_file, tmp_folder) {
+download_sb_files <- function(target_name, sb_id, sb_filenames_file, dest_folder) {
+  
+  sb_check_login()
   
   sb_filenames <- readRDS(sb_filenames_file)
   
   local_filenames <- item_file_download(
     sb_id, 
     names = sb_filenames, 
-    destinations = file.path(tmp_folder, sb_filenames))
+    destinations = file.path(dest_folder, sb_filenames),
+    overwrite_file = TRUE)
   
   saveRDS(object = local_filenames, file = target_name)
+}
+
+download_sb_single_file <- function(target_name, sb_id, sb_filename) {
+  
+  sb_check_login()
+  
+  local_filename <- item_file_download(
+    sb_id, 
+    names = sb_filename, 
+    destinations = target_name, 
+    overwrite_file = TRUE)
+  
+}
+
+sb_check_login <- function() {
+  if (!sbtools::is_logged_in()){
+    sb_secret <- dssecrets::get_dssecret("cidamanager-sb-srvc-acct")
+    sbtools::authenticate_sb(username = sb_secret$username, password = sb_secret$password)
+  }
 }
