a4f77532a99c803b4163a7ad635a57a7ca104755
Platt <lplatt@usgs.gov>
2020-04-21 10:11:52 -0500

    describe enframe a bit more

--- 3_summarize/src/calculate_total_benthic_area.R
+++ 3_summarize/src/calculate_total_benthic_area.R
@@ -5,6 +5,7 @@ calculate_total_benthic_area <- function(target_name, all_hypsos) {
     sum(benthic_areas(hypso$H, hypso$A))
   }) %>%
     # `enframe` required me to update tibble (I had `2.1.3` and now have `3.0.1`)
+    # Takes a list and creates a two-column data.frame (one column is the names, one is the values)
     tibble::enframe(name = "site_id", value = "max_benthic_area") %>% 
     tidyr::unnest(max_benthic_area) %>% 
     readr::write_csv(target_name)
