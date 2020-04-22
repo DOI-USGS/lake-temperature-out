a9f664e1d2ac5a15b938c7c418904604424bf4ba
Jordan S Read <jread@usgs.gov>
2020-04-22 05:53:54 -0500

    updating column name

--- 3_summarize/src/calculate_total_benthic_area.R
+++ 3_summarize/src/calculate_total_benthic_area.R
@@ -6,8 +6,8 @@ calculate_total_benthic_area <- function(target_name, all_hypsos) {
   }) %>%
     # `enframe` required me to update tibble (I had `2.1.3` and now have `3.0.1`)
     # Takes a list and creates a two-column data.frame (one column is the names, one is the values)
-    tibble::enframe(name = "site_id", value = "max_benthic_area") %>% 
-    tidyr::unnest(max_benthic_area) %>% 
+    tibble::enframe(name = "site_id", value = "total_benthic_area") %>% 
+    tidyr::unnest(total_benthic_area) %>% 
     readr::write_csv(target_name)
   
 }
