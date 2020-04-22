dedda7071b10937a79658f65e0477a97f3c29a1f
Platt <lplatt@usgs.gov>
2020-04-22 11:39:17 -0500

    move height translation to depth into calculate_total_benthic_area

--- 3_summarize/src/calculate_total_benthic_area.R
+++ 3_summarize/src/calculate_total_benthic_area.R
@@ -1,24 +1,14 @@
 
-calculate_total_benthic_area <- function(target_name, all_hypsos) {
+calculate_total_benthic_area <- function(target_name, morphometry) {
   
-  purrr::map(all_hypsos, function(hypso) {
-    sum(benthic_areas(hypso$depths, hypso$areas))
+  purrr::map(morphometry, function(m) {
+    # Order of depths & areas will be fixed in benthic_areas
+    sum(benthic_areas(depths = max(m$H) - m$H, areas = m$A))
   }) %>%
     # `enframe` required me to update tibble (I had `2.1.3` and now have `3.0.1`)
     # Takes a list and creates a two-column data.frame (one column is the names, one is the values)
     tibble::enframe(name = "site_id", value = "max_benthic_area") %>% 
     tidyr::unnest(max_benthic_area) %>% 
     readr::write_csv(target_name)
-  
-}
-
-extract_hypsography <- function(morphometry) {
-  # Only keep depths (converted from Heights, H) and areas (A)
-  purrr::map(morphometry,  function(m) {
-    hypsos <- data.frame(H = m$H, A = m$A) %>% 
-      mutate(depths = max(H) - H, areas = A) %>% 
-      arrange(depths) %>% 
-      select(depths, areas)
-  })
   
 }
