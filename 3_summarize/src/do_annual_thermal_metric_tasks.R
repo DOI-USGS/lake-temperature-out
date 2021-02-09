d08aad6cb7b1b3a62677561bf2037b767b0f1f7b
Platt <lplatt@usgs.gov>
2021-02-09 08:57:24 -0600

    pass ymls into function so that hashes aren't dropped before remake decides to rebuild or not

--- 3_summarize/src/do_annual_thermal_metric_tasks.R
+++ 3_summarize/src/do_annual_thermal_metric_tasks.R
@@ -1,6 +1,9 @@
 
-do_annual_metrics_multi_lake <- function(final_target, site_files, ice_files, n_cores, morphometry, temp_ranges, ...,
+do_annual_metrics_multi_lake <- function(final_target, site_file_yml, ice_file_yml, n_cores, morphometry, temp_ranges, ...,
                                          site_file_regex = NULL, ice_file_regex = NULL, tmpdir_suffix = "") {
+  
+  site_files <- get_filenames_from_ind(site_file_yml)
+  ice_files <- get_filenames_from_ind(ice_file_yml)
   
   # Each node on a Yeti normal partition has a max of 20 cores; nodes on Yeti UV partition do not have that same limit
   if(n_cores > 20) message("If using a node on the Yeti normal partition, you need to decrease n_cores to 20 or less")
