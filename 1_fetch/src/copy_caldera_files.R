7934ec2f631d422a4594240c126e01e0f11ffbe5
Platt <lplatt@usgs.gov>
2022-02-16 16:43:55 -0600

    need copy fxn

---
+++ 1_fetch/src/copy_caldera_files.R
@@ -0,0 +1,13 @@
+
+copy_caldera_files <- function(out_ind, files_in, out_dir = NULL) {
+  
+  if(is.null(out_dir)) {
+    files_out <- as_data_file(out_ind)
+  } else {
+    files_out <- file.path(out_dir, basename(files_in))
+  }
+  
+  file.copy(from = files_in, to = files_out)
+  sc_indicate(out_ind, data_file = files_out)
+  
+}
