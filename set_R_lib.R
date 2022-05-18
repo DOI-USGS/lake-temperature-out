9cd10ad25dbf8e0cdab486c0f0b1e27caba6a8ee
Lindsay Platt <lplatt@igskmncnpshtl01.tallgrass.cr.usgs.gov>
2022-05-18 15:45:08 -0500

    relocate where the Renviron variable for libs is set to not interfere with singularity

---
+++ set_R_lib.R
@@ -0,0 +1,3 @@
+# Use this script to set the environment variable. Needed if you are running without a container.
+Sys.setenv("R_LIBS_USER" = c("/caldera/projects/usgs/water/iidd/datasci/lake-temp/lake-temperature-out/Rlib_3_6", "/home/software/tallgrass/arc/apps/R/3.6.3/lib64/R/library"))
+
