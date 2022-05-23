cb8dd1226dbe59c6a0512dad2101dfb19473c04b
Lindsay Platt <lplatt@igskmncnpshtl01.tallgrass.cr.usgs.gov>
2022-05-23 15:25:05 -0500

    Change path setup for a quick way to install packages when in an interactive session

--- set_R_lib.R
+++ set_R_lib.R
@@ -1,3 +1,5 @@
 # Use this script to set the environment variable. Needed if you are running without a container.
-Sys.setenv("R_LIBS_USER" = c("/caldera/projects/usgs/water/iidd/datasci/lake-temp/lake-temperature-out/Rlib_3_6", "/home/software/tallgrass/arc/apps/R/3.6.3/lib64/R/library"))
+libpath <- "/caldera/projects/usgs/water/iidd/datasci/lake-temp/lake-temperature-out/Rlib_3_6"
+Sys.setenv("R_LIBS_USER" = libpath)
+.libPaths(libpath)
 
