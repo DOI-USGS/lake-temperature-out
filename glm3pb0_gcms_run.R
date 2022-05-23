f7ae5e019caf74b8deb0ca5598af06bf43fb2911
Lindsay Platt <lplatt@igskmncnpshtl01.tallgrass.cr.usgs.gov>
2022-05-23 15:23:03 -0500

    changes from renaming & to get builds working

--- glm3pb0_gcms_run.R
+++ glm3pb0_gcms_run.R
@@ -2,6 +2,6 @@ library(scipiper)
 
 message(sprintf("starting 3_summarize for glm3pb0 at %s", Sys.time()))
 
-scmake("3_summarize/out/annual_metrics_glm3pb0_gcms.csv")
+scmake("3_summarize/out/annual_metrics_glm3_pb0gcm.csv")
 
 message(sprintf("finished 3_summarize for glm3pb0 at %s", Sys.time()))
